2aea960634e59ffc43e04c3faa32fec7fa5ff284
VietLinh1104 <linhtx2004@gmail.com>
2026-03-11 00:03:56 +0700

    first commit

---
+++ src/main/java/com/linhdv/efms_core_service/EfmsCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.linhdv.efms_core_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfmsCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfmsCoreServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linhdv/efms_core_service/EfmsCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linhdv.efms_core_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfmsCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
